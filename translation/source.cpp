462bbb202c3b8c5b86a3026523e30617d6a5bfb1
David Jäderberg <david@jaderberg.net>
2014-07-23 15:27:15 +0200

    Add source.h and source.cpp

---
+++ translation/source.cpp
@@ -0,0 +1,11 @@
+#include "source.h"
+
+//Empty because implementations of unspecialized templates can not be defined in 
+//a file that is invisible to some translation unit that uses the template.
+//I.e., since source.h only has template classes, all implementation details must
+//be kept in source.h. 
+//
+//More info at:
+//http://stackoverflow.com/a/12574417
+//http://stackoverflow.com/q/495021
+
