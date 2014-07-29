779a1fa980ef445f3ffd579a9a2d1cf6113ec1c1
David Jäderberg <david@jaderberg.net>
2014-07-29 15:47:43 +0200

    Add two files used for testing

---
+++ test/expressions.c
@@ -0,0 +1,49 @@
+a, b;
+sum = a + b; //Comment
+mult *= a - b;
+div /= a - b;
+mod %= a -b;
+add += a - b;
+sub -= a - b;
+lsh <<= a - b;
+rsh >>= a - b;
+ads &= a - b;
+xor ^= a - b;
+or |= a - b;
+cond = true ? true : false; //Seems to mess up memory sometimes
+LOR = a || b;
+LAND = a && b;
+BOR = a | b;
+BXOR = a ^ b;
+BAND = a & b;
+a == b;
+a != b;
+a < b;
+a > b;
+a <= b;
+a >= b;
+a << b;
+a >> b;
+a + b;
+a - b;
+a * b;
+a / b;
+a % b;
+intType = (int) a + b;
++a; //Integer promotion
+-(int)a;
+&a;
+*a;
+~a;
+!a;
+++a;
+--a;
+sizeof a;
+sizeof(int);
+_Alignof(int);
+a++;
+a--;
+a->b;
+a.b;
+paren = (a + b) * c;
+
